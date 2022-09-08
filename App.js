9996c0ace70fd88495980c99d2b0c0ba9bab194d
Kylle <caimcd.dutra@hotmail.com>
2022-09-08 00:04:53 -0300

    aulas de react

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {StatusBar, StyleSheet, SafeAreaView} from 'react-native';
+import Cesta from './src/telas/cesta'
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Cesta />
+    </SafeAreaView>
+  );
+}
+
+
